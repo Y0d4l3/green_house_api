737a320f3a709113cbe8b22a4f6107c6caf5cda0
Yodale <yodale@tutanota.com>
2023-05-01 09:15:03 +0200

    update

--- controllers/device.js
+++ controllers/device.js
@@ -3,40 +3,46 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.getDevices = async (req, res) => {
-  Device.find({ user: req.user.id })
-    .then(function (devices) {
-      res.status(200).json(devices);
-    })
-    .catch(function (err) {
-      res.status(500).json(err);
-    });
+  try {
+    const devices = Device.find({ user: req.user.id });
+    if (!devices) {
+      res.status(404).json({ message: "No devices found" });
+      return;
+    }
+    res.status(200).json(devices);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
-exports.createDevice = (req, res) => {
-  Device.findOne({ name: req.body.name, user: req.user.id })
-    .then((existingDevice) => {
-      if (existingDevice) {
-        return res
-          .status(409)
-          .json({ message: "device with the same name already exists" });
-      }
-      const newDevice = new Device({
-        name: req.body.name,
-        location: req.body.location,
-        user: req.user.id,
-      });
-      apiToken = jwt.sign({ deviceId: newDevice.id }, process.env.JWT_SECRET, {
-        expiresIn: "365d",
-      });
-      newDevice.apiToken = apiToken;
-      newDevice
-        .save()
-        .then((savedDevice) => {
-          res.status(201).json(savedDevice);
-        })
-        .catch((err) => res.status(500).json(err));
-    })
-    .catch((err) => res.status(500).json(err));
+exports.createDevice = async (req, res) => {
+  try {
+    const existingDevice = await Device.findOne({
+      name: req.body.name,
+      user: req.user.id,
+    });
+    if (existingDevice) {
+      res.status(404).json({ message: "Device with that name already exists" });
+      return;
+    }
+    const newDevice = new Device({
+      name: req.body.name,
+      location: req.body.location,
+      user: req.user.id,
+    });
+    apiToken = jwt.sign({ deviceId: newDevice.id }, process.env.JWT_SECRET, {
+      expiresIn: "365d",
+    });
+    newDevice.apiToken = apiToken;
+    const savedDevice = await newDevice.save();
+    if (!savedDevice) {
+      res.status(404).json({ message: "Device could not be saved" });
+      return;
+    }
+    res.status(201).json(savedDevice);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 exports.updateDevice = async (req, res) => {
