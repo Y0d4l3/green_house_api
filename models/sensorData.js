25c8ccbbe04e83f48ea18a88d5d8bfb490d6309e
Yodale <yodale@tutanota.com>
2023-05-07 19:13:32 +0200

    fix

--- models/sensorData.js
+++ models/sensorData.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const request = require("request");
-require("dotenv").config();
 
 const temperatureReadingSchema = new mongoose.Schema({
   value: { type: Number, required: true },
@@ -31,35 +29,3 @@ const sensorDataSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("SensorData", sensorDataSchema);
-
-SensorData.post("save", async function (doc, next) {
-  console.log("test");
-  try {
-    const latestSensorData = await SensorData.find()
-      .sort("-timestamp")
-      .limit(2);
-    const latestTemperatureReading =
-      latestSensorData[0].temperatureReading.value;
-    const previousTemperatureReading =
-      latestSensorData[1].temperatureReading.value;
-
-    if (
-      Math.abs(latestTemperatureReading - previousTemperatureReading) >
-      process.env.TEMPERATURE_TRESHHOLD
-    ) {
-      request(process.env.IFTTT_URI, { json: true }, (err, res, body) => {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(body.url);
-        console.log(body.explanation);
-      });
-      return next();
-    }
-
-    next();
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
